e3743ddcf67c8e47d34ae8e168c93938e094aea3
JoinedRobbin1887 <eduterbo@gmail.com>
2025-11-22 12:47:58 +0100

    feat:Nouse

--- frontend/joc-de-barris-front/src/components/Button.js
+++ frontend/joc-de-barris-front/src/components/Button.js
@@ -1,1 +1,19 @@
-import React from 'react';
+import React from "react";
+
+export function Button({ children, className = "", ...props }) {
+  return (
+    <button
+      type="submit"
+      className={`
+        flex items-center justify-center rounded-3xl bg-indigo-500
+        cursor-pointer
+        text-sm leading-6 font-semibold
+        text-white hover:bg-indigo-400
+        focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500
+        ${className}`}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+}
