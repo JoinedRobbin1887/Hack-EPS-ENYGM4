24a1acecc75e1a91ae5e85056df183ba8926ebaa
JoinedRobbin1887 <eduterbo@gmail.com>
2025-11-22 22:29:22 +0100

    feat:THINGS

--- frontend/joc-de-barris-front/src/components/Economia.js
+++ frontend/joc-de-barris-front/src/components/Economia.js
@@ -11,7 +11,7 @@ export function Economia() {
   return (
     <div className="w-full mb-6 p-4 bg-white rounded-lg shadow">
       <h2 className="font-bold text-xl mb-2">Demografía y Economía</h2>
-      <PreferenceSelector label="INGRESOS" options={["Rango 1", "Rango 2", "Rango 3"]} selected={ingresos} onChange={setIngresos} />
+      <PreferenceSelector label="INGRESOS" options={["fking mileurista", "normalito", "élite"]} selected={ingresos} onChange={setIngresos} />
       <PreferenceSelector label="EDAD" options={["Rango 1", "Rango 2", "Rango 3"]} selected={edad} onChange={setEdad} />
       <PreferenceSelector label="DENSITAT" options={["Rango 1", "Rango 2", "Rango 3"]} selected={densitat} onChange={setDensitat} />
       <PreferenceSelector label="ACTIVITAT ECONOMICA" options={["Rango 1", "Rango 2", "Rango 3"]} selected={activitat} onChange={setActivitat} />
