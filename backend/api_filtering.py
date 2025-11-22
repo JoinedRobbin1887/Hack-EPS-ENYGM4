c77ccb168145a89a9b78c18438f332f4dbfa8801
nicole <nickiissj@gmail.com>
2025-11-22 19:25:14 +0100

    refactor: :memo: improve logic

--- backend/api_filtering.py
+++ backend/api_filtering.py
@@ -4,33 +4,37 @@ import requests
 #1 - PRIMERA FILTRACIÓN NO PONDERADA - datos fiables
 
 #Construimos la query según los filtros de la ciudad
-def build_overpass_query(filters, city="Los Angeles"):
-    
+def build_overpass_query(filters : dict[str, bool], city : str = "Los Angeles"):
+
     blocks = []
 
-    # --- ESTILO DE VIDA ---
-    if filters.get("restaurants"):
-        blocks.append('node["amenity"="restaurant"](area.searchArea);')
-    if filters.get("parks"):
-        blocks.append('node["leisure"="park"](area.searchArea);')
-    if filters.get("cultural"):
-        blocks.append('node["amenity"~"theatre|library|museum"](area.searchArea);')
-    if filters.get("gyms"):
-        blocks.append('node["leisure"="fitness_centre"](area.searchArea);')
-        blocks.append('node["sport"="fitness"](area.searchArea);')
-    if filters.get("shops"):
-        blocks.append('node["shop"](area.searchArea);')
+    query_struct = {
+        # --- ESTILO DE VIDA ---
+        "restaurants": ['node["amenity"="restaurant"](area.searchArea);'],
+        "parks": ['node["leisure"="park"](area.searchArea);'],
+        "cultural": ['node["amenity"~"theatre|library|museum"](area.searchArea);'],
+        "gyms": [
+            'node["leisure"="fitness_centre"](area.searchArea);',
+            'node["sport"="fitness"](area.searchArea);'
+        ],
+        "shops": ['node["shop"](area.searchArea);'],
 
-     # --- MOVILIDAD Y TRANSPORTE ---
-    if filters.get("sidewalks"):
-        blocks.append('way["sidewalk"="both"](area.searchArea);')
-    if filters.get("public_transport"):
-        blocks.append('node["bus"="yes"](area.searchArea);')
-        blocks.append('node["train"="yes"](area.searchArea);')
-    if filters.get("accessibility"):
-        blocks.append('node["wheelchair"="yes"](area.searchArea);')
-        blocks.append('way["wheelchair"="yes"](area.searchArea);')
-        blocks.append('way["sidewalk"](area.searchArea);')
+        # --- MOVILIDAD Y TRANSPORTE ---
+        "sidewalks": ['way["sidewalk"="both"](area.searchArea);'],
+        "public_transport": [
+            'node["bus"="yes"](area.searchArea);',
+            'node["train"="yes"](area.searchArea);'
+        ],
+        "accessibility": [
+            'node["wheelchair"="yes"](area.searchArea);',
+            'way["wheelchair"="yes"](area.searchArea);',
+            'way["sidewalk"](area.searchArea);'
+        ]
+    }
+
+    for feature, block_list in query_struct.items():
+        if filters.get(feature):
+            blocks.extend(block_list)
 
     # Si no hay ningún bloque activo
     if not blocks:
@@ -42,14 +46,14 @@ def build_overpass_query(filters, city="Los Angeles"):
         [out:json][timeout:50];
         area["name"="{city}"]->.searchArea;
         (
-                {blocks_joined}
+        {blocks_joined}
         );
         out center;
         """
     return query
 
 #llamamos a la api
-def call_overpass(query):
+def call_overpass(query ):
     if not query:
         return None
     url = "https://overpass-api.de/api/interpreter"
@@ -85,3 +89,13 @@ print(query)
 print("\nPrimeros 3 elementos encontrados:")
 for i, elem in enumerate(data["elements"][:3], start=1):
     print(f"{i}: {elem}")
+    
+
+def ponder_characteristics (data):
+    raise NotImplementedError ("No hecho aun")
+
+def sum_scores(data):
+    raise NotImplementedError ("No hecho aun")
+
+def best_neighboord(data):
+    raise NotImplementedError ("No hecho aun")
