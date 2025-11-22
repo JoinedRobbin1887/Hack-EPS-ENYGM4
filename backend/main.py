5360640d9e950b8dcaa293eca66ba6a954a67dfe
Gerard <gerymaps200515@gmail.com>
2025-11-22 22:35:15 +0100

    build: refact main api

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from routers.forms import Demografia, EstilVida, Habitatge, Movilitat, Seguridad
+from api_filtering import build_overpass_query
 
 app = FastAPI()
 
@@ -10,3 +11,32 @@ app.include_router(Habitatge.router)
 app.include_router(Movilitat.router)
 app.include_router(Seguridad.router)
 
+
+@app.post("/formcomplite")
+def get_form(form: dict):
+    demografia= form["demografia"]
+    estatvida= form["vida"]
+    seguretat= form["seguretat"]
+    habitatge= form["habitatge"]
+    movilitat= form["movilitat"]
+    prioritat= form["prioritat"]
+
+    estatvidaMovilitat = estatvida | movilitat
+
+    print(prioritat)
+
+    new_priority = reord_priority(prioritat)
+    build_overpass_query(estatvidaMovilitat)
+    print(new_priority)
+
+    return estatvidaMovilitat
+
+
+
+def reord_priority(prioritat: dict):
+    
+    new_prioritys = {}
+
+    for key, value in prioritat.items():
+        new_prioritys[key] = value + 1
+    return new_prioritys
